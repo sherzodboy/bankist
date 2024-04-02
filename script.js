66fc35fc0a462f6a6b8b502080a153e8f48d2a58
sherzodboy <kholmamatovsh09@gmail.com>
2024-04-02 09:59:22 +0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,226 @@
+'use strict';
+
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayMovements = (movements) => {
+  containerMovements.innerHTML = '';
+  movements.forEach((move, idx) => {
+    const type = move > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      idx + 1
+    } ${type}</div>
+        <div class="movements__value">${move}</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+**Coding Challenge #1
+
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners
+about their dog's age, and stored the data into an array (one array for each). For
+now, they are just interested in knowing whether a dog is an adult or a puppy.
+A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years
+old.
+
+**Your tasks:**
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages
+('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the **first** and the **last two** dogs actually have
+cats, not dogs! So create a shallow copy of Julia's array, and remove the cat
+ages from that copied array (because it's a bad practice to mutate function
+parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult (*"Dog number 1 is an adult, and is 5 years old"*) or a puppy (*"Dog number 2 is still a puppy ")
+    
+4. Run the function for both test datasets
+
+**Test data:**
+
+§ Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+§ Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+**Hints:** Use tools from all lectures in this section so far 😉
+**GOOD LUCK** 😀
+
+const checkDogs = (dogsJulia, dogsKate) => {
+  const correctedDogsJulia = dogsJulia.slice();
+  correctedDogsJulia.splice(0, 1);
+  correctedDogsJulia.splice(-2);
+  
+  const dogs = correctedDogsJulia.concat(dogsKate);
+  console.log(dogs);
+  
+  dogs.forEach((age, i) => {
+    if (age >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy`);
+    }
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+*/
+
+// LECTURES
+
+/*
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, a) {
+  console.log(`${key}: ${value} `);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'EUR', 'USD', 'GBP', 'EUR']);
+console.log(currenciesUnique);
+
+currenciesUnique.forEach((value, _, arr) => {
+  console.log(value);
+});
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`${i + 1}: You withdrawl ${Math.abs(movement)}`);
+  }
+}
+
+console.log('-------forEach------');
+
+movements.forEach((move, idx, arr) => {
+  if (move > 0) {
+    console.log(`${idx + 1}: You deposited ${move}`);
+  } else {
+    console.log(`${idx + 1}: You withdrawl ${Math.abs(move)}`);
+  }
+});
+*/
+
+/////////////////////////////////////////////////
+
+/*
+// Slice
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+console.log('-------------');
+
+// Splice
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+
+arr.splice(1, 2);
+console.log(arr);
+
+// Reverse
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['d', 'a', 'n', 'i', 'e', 'l'];
+
+console.log(arr2.reverse());
+console.log(arr2);
+
+// Concat
+let letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// Join
+console.log(letters.join(' - '));
+
+// At
+let arr = [12, 34, 68];
+
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+*/
